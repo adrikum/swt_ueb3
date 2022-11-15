e91da930751b7268e144ad2ed1b6abb18aaac9da
tuan-nguye <70942348+tuan-nguye@users.noreply.github.com>
2022-11-15 10:49:34 +0100

    edited readme, pom.xml

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # swt_ueb3
+
+3
+- über IntelliJ Idea ein Maven Projekt erstellt
+- Dateistruktur automatisch erstellt, sowie pom.xml
+- IntellijJ-Maven Plugin ermöglicht Befehle wie 'compile' für Kombilieren, 'test' für Testvorgänge über Knopfdruck
+- alternativ auch über eingebaute konsole möglich, z.B. 'mvn test'
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,24 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        System.out.println("size: " + list.size());
+        Iterator<Integer> it = list.iterator();
+        while(it.hasNext()) {
+            int i = it.next();
+            System.out.println(i);
+            it.remove();
+        }
+        System.out.println("size: " + list.size());
     }
 }
