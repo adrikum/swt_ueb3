2d2577fb695aa421416bd6c9d6d282d976ca1d14
tuan-nguye <70942348+tuan-nguye@users.noreply.github.com>
2022-11-29 09:59:53 +0100

    added size after removal test

--- src/test/java/list/tests/SizeTest.java
+++ src/test/java/list/tests/SizeTest.java
@@ -3,6 +3,9 @@ package list.tests;
 import list.SimpleLinkedList;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.Iterator;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /*
@@ -20,4 +23,18 @@ public class SizeTest {
         assertEquals(3,list.size());
     }
 
+    @Test
+    public void sizeAfterRemove() {
+        SimpleLinkedList<Integer> list = new SimpleLinkedList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        for(Iterator<Integer> it = list.iterator(); it.hasNext();) {
+            it.next();
+            it.remove();
+        }
+
+        assertEquals(0, list.size());
+    }
 }
